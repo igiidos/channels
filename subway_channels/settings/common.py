f5e7767d873098194cbbd06576770cddf2de7387
igiidos <igiidos@gmail.com>
2019-08-15 20:20:47 +0900

    셋팅즈에 스테틱과 메디아 루트 지정

--- subway_channels/settings/common.py
+++ subway_channels/settings/common.py
@@ -130,3 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+] #add static folder
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
